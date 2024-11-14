4a80d04228da3c733b71c6b8a9d4f1eed1b996c3
jaekusuma <jaekusuma@gmail.com>
2024-11-14 12:23:30 +0700

    add meme for fun

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
-const toggle = document.getElementById('modeToggle');
+// const toggle = document.getElementById('modeToggle');
 
-toggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
-});
+// toggle.addEventListener('change', () => {
+//     document.body.classList.toggle('dark-mode');
+// });
 
 const blinkingText = document.getElementById('belonJadi');
     
-        function getRandomColor() {
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
         }
+    return color;
+}
     
-        setInterval(() => {
-            blinkingText.style.color = getRandomColor();
-            document.body.style.backgroundColor = getRandomColor();
-        }, 100);
+setInterval(() => {
+    blinkingText.style.color = getRandomColor();
+    document.body.style.backgroundColor = getRandomColor();
+}, 100);
